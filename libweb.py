7efa69c3fa9e1e6a357bb99c4c23cf49246cb9e4
liuyix <hi@liuyix.org>
2013-08-05 23:50:39 +0800

    实现了logout,forcelogout

--- libweb.py
+++ libweb.py
@@ -19,7 +19,11 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 POST_LOGIN_URL = "http://10.0.0.55/cgi-bin/do_login"
+POST_LOGOUT_URL = "http://10.0.0.55/cgi-bin/do_logout"
+POST_FORCE_LOGOUT_URL = "http://10.0.0.55/cgi-bin/force_logout"
 
+
+LOGIN_SESSIONID = ""
 
 # 若要测试，需要将个人的用户名和密码以及post数据中md5加密后的密码串放入.user_info中
 def get_user_info(filename=".user_info"):
@@ -31,7 +35,6 @@ def get_user_info(filename=".user_info"):
         result = "username=%s&password=%s&drop=0&type=1&n=100" %(info_list[0], info_list[2])
         return {"username": info_list[0], "password": info_list[1], "result": result}
     return None
-            
 
 def get_login_data(username, passwd):
     assert username != None and passwd != None
@@ -42,23 +45,64 @@ def get_login_data(username, passwd):
     post_data = "username=%(username)s&password=%(password)s&drop=0&type=1&n=100" % {"username": username, "password": passwd_phase2}
     logging.info("post_data: %s", post_data)
     return post_data
-    
+
+def do_post(url, data):
+    assert url != None
+    request = urllib2.Request(url, data, headers={"Content-Type": "application/x-www-form-urlencoded"})
+    response_file = urllib2.urlopen(request)
+    return response_file
+
 def login(username, password):
     assert username != None and password != None
     data = get_login_data(username, password)
-    req = urllib2.Request(POST_LOGIN_URL, data, headers={"Content-Type": "application/x-www-form-urlencoded"})
-    f = urllib2.urlopen(req)
-
+    f = do_post(POST_LOGIN_URL, data)
     for response_data in f:
         print response_data
+        if response_data.isdigit(): # login success
+            global LOGIN_SESSIONID
+            LOGIN_SESSIONID = response_data
+            logging.info("login success, session id: %s" % (LOGIN_SESSIONID ))
         return response_data
-        
-def test_post_data():
 
-    test_case = get_user_info()
-    data = get_login_data(test_case['username'], test_case['password'])
+def do_force_logout(username, password):
+    assert username != None and password != None
+    flogout_post_data = "username=%(username)s&password=%(password)s&drop=0&n=1" % {'username': username, 'password': password}
+    response = do_post(POST_FORCE_LOGOUT_URL, flogout_post_data)
+    for data in response:
+        return data
+    pass
+
+def logout(username, password, force=False):
+    if force:
+        return do_force_logout(username, password)
+    else:
+        if LOGIN_SESSIONID != None and LOGIN_SESSIONID != "":
+            logout_post_data = "uid=%s" % (LOGIN_SESSIONID)
+            response = do_post(POST_LOGOUT_URL, logout_post_data)
+            for data in response:
+                return data
+        else:
+            logging.error("SESSION ID is invalid!")
+            return None
+
+def test_force_logout():
+    user_info = get_user_info()
+    data = logout(user_info['username'], user_info['password'], force=True)
+    print data
+    
+def test_logout():
+    user_info = get_user_info()
+    return_data = login(user_info['username'],user_info['password'])
+    if return_data.isdigit():
+        logout_status = logout(user_info['username'], user_info['password'], force=False)
+        print logout_status
+    
+
+def test_post_data():
+    user_info = get_user_info()
+    data = get_login_data(user_info['username'], user_info['password'])
     logging.debug("post_data: %s", data)
-    assert data == test_case['result']
+    assert data == user_info['result']
     print "PASS"
             
 def test_login():
@@ -71,6 +115,8 @@ def test_login():
     print "ALL PASS"
     
 if __name__ == "__main__":
-    test_post_data()
-    test_login()
+    #test_post_data()
+    #test_login()
+    #test_logout()
+    test_force_logout()
     
